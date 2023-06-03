6d9bf1ace31e351b790deb38cf231112180fea29
Gavin Pavlick <gavin.pavlick@kellogg.northwestern.edu>
2023-06-03 18:08:12 +0000

    fourth commit

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -1,5 +1,6 @@
 secret = "password"
 encrypted_secret = BCrypt::Password.create(secret)
 puts encrypted_secret
-
 user_entered_secret = "password"
+result = BCrypt::Password.new(encrypted_secret) == user_entered_secret
+puts result
