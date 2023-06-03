b5ba0a4952b9dbfa1be332d4653966bde406ea76
Gavin Pavlick <gavin.pavlick@kellogg.northwestern.edu>
2023-06-03 14:43:51 +0000

    first commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      redirect to "/places"
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
   end
 
   def destroy
